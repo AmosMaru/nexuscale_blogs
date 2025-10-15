4f30f0d7e921657846096060f70701b85ac1ebe1
AmosMaru <amosmaru10@gmail.com>
2025-10-15 15:12:13 +0300

    add origin url

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Nexuscale Articles API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", 'https://nexuscale.ai', "https://www.nexuscale.ai"],  # or ["*"] for all
+    allow_origins=["http://localhost:5173", 'https://nexuscale.ai', "https://www.nexuscale.ai","https://articles.nexuscale.ai" ],  # or ["*"] for all
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
